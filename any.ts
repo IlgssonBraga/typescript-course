ae259b81e64e329c6efb979e084a1ebc8ba062b8
Ilgsson Braga <ilgssonbraga@gmail.com>
2020-07-06 09:12:51 -0300

    feat: any

---
+++ any.ts
@@ -0,0 +1,13 @@
+let value: any = "Hello";
+
+console.log(value);
+
+value = true;
+
+console.log(value);
+
+value = 17;
+
+console.log(value);
+
+// When we use any type, we stay very close to the javascript code
