36464586cad35fe3dcd36da9febfb98a8d7bd924
Ilgsson Braga <ilgssonbraga@gmail.com>
2020-07-06 09:29:49 -0300

    feat: objects

---
+++ objects.ts
@@ -0,0 +1,17 @@
+const person = {
+  name: "Ilgsson",
+  age: 25,
+};
+
+console.log(person);
+
+// person.surname = "Braga";
+
+// console.log(person);
+
+const person2: { name: string; age: number } = {
+  name: "Arlaine",
+  age: 24,
+};
+
+console.log(person2);
