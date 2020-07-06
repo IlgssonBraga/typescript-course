eea97fe9058a970a85840de2c698e550df673115
Ilgsson Braga <ilgssonbraga@gmail.com>
2020-07-06 09:18:56 -0300

    feat: functions

---
+++ functions.ts
@@ -0,0 +1,11 @@
+function test(name: string): void {
+  console.log(name);
+}
+
+test("Ilgsson");
+
+function mult(num1: number, num2: number): number {
+  return num1 * num2;
+}
+
+console.log(mult(4, 3));
