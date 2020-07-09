2f29aeaeb781074e46f93aa015bba656d138c32a
Ilgsson Braga <ilgssonbraga@gmail.com>
2020-07-09 10:05:52 -0300

    feat: spread operator

---
+++ ES6/spread.ts
@@ -0,0 +1,9 @@
+const turmaA = ["João", "Maria", "José"];
+
+const escola = ["Luiz", "Josefa", ...turmaA];
+
+console.log(escola);
+
+const numbers = [1, 3, 99, -2, 10, 59, 1024, 3535, 7000];
+
+console.log(Math.max(...numbers));
