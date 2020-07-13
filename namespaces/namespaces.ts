68a43dcf2cb941459556fe64e2cce78ee18b1b34
Ilgsson Braga <ilgssonbraga@gmail.com>
2020-07-13 15:23:53 -0300

    feat: nested namespaces

--- namespaces/namespaces.ts
+++ namespaces/namespaces.ts
@@ -1,13 +1,15 @@
-namespace Areas {
-  const PI = 3.14;
-  export function circ(radius: number): number {
-    return PI * Math.pow(radius, 2);
-  }
+namespace Geometria {
+  export namespace Areas {
+    const PI = 3.14;
+    export function circ(radius: number): number {
+      return PI * Math.pow(radius, 2);
+    }
 
-  export function rect(base: number, high: number): number {
-    return base * high;
+    export function rect(base: number, high: number): number {
+      return base * high;
+    }
   }
 }
 
-console.log(Areas.circ(10));
-console.log(Areas.rect(10, 20));
+console.log(Geometria.Areas.circ(10));
+console.log(Geometria.Areas.rect(10, 20));
