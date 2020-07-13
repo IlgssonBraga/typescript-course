0e4defbfb7259e991332804aab5f8e7b5644196f
Ilgsson Braga <ilgssonbraga@gmail.com>
2020-07-13 09:24:40 -0300

    feat: class without attributes

---
+++ classes/classes02.ts
@@ -0,0 +1,8 @@
+class ExpertDate {
+  constructor(public day: number, public month: number, public year: number) {
+  }
+}
+
+const expertBirthday = new ExpertDate(3, 9, 1994);
+
+console.log(expertBirthday);
